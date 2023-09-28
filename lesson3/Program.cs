a61677d9226f9b6bd51bd42eb90dd211f4205455
aigulnazipova <143903082+aigulnazipova@users.noreply.github.com>
2023-09-28 23:55:25 +0300

    Update Program.cs

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -9,16 +9,7 @@ namespace lesson3
 {
     internal class Program
     {
-        enum DayOfWeek
-        {
-            Monday = 1,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
-        }
+        
         static void Main(string[] args)
         {
             
